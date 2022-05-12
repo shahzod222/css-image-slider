bb17b4270715538ffac4b0cbc52932c4218564c4
Shahzod222 <shahzodubaydullaev2206@gmail.com>
2022-05-12 20:53:52 +0500

    fix: animation was finally fixed (Thu, May 12, 2022, 08:53:12PM)

--- cssMemSlider/app.js
+++ cssMemSlider/app.js
@@ -1,16 +1,14 @@
 let par = document.querySelector(".text");
 let img = document.querySelector(".img");
-let allImgs = [
+let allImgAssets = [
   { src: "assets/bmw.jpg", title: "BMW X5" },
   { src: "assets/mers.jfif", title: "Mercedes-Benz G-Class AMG" },
   { src: "assets/cadillac.jfif", title: "Cadillac Escalade" },
   { src: "assets/tesla.jpg", title: "Tesla Model X" },
   { src: "assets/tahoe.jpg", title: "Chevrolet Tahoe" },
 ];
-
 let current = 0;
 let previous = 0;
-
 let allButtons = document.getElementsByName("btn");
 let allButtonWrappers = document.querySelectorAll(".btn-wrapper");
 allButtons.forEach((el) => {
@@ -25,27 +23,20 @@ allButtonWrappers.forEach((el) => {
 });
 function showHtml(element) {
   current = element.id;
-
   allButtons[previous].classList.remove("active");
   allButtons[previous].classList.add("btn");
   allButtons[current].classList.remove("btn");
   allButtons[current].classList.add("active");
-  previous = current;
-  img.classList.add("disappear");
-  par.classList.add("disappear");
+  img.style.opacity = "0";
+  par.style.opacity = "0";
   setTimeout(() => {
-    img.src = allImgs[element.id].src;
-    par.innerHTML = allImgs[element.id].title;
-  }, 150);
+    img.style.background = `url(${allImgAssets[current].src}) no-repeat`;
+    img.style.backgroundSize = "cover";
+    par.innerHTML = allImgAssets[current].title;
+  }, 400);
   setTimeout(() => {
-    img.classList.remove("disappear");
-    par.classList.remove("disappear");
-    img.classList.add("appear");
-    par.classList.add("appear");
-  }, 300);
-
-  setTimeout(() => {
-    img.classList.remove("appear");
-    par.classList.remove("appear");
+    img.style.opacity = "1";
+    par.style.opacity = "1";
   }, 600);
+  previous = current;
 }
